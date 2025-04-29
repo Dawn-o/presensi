cb43915bd7e85c98f289577a9c580b65a7d5ead4
Rushel <ruselaq@gmail.com>
2025-04-29 11:23:42 +0800

    styles: make Daftar Karyawan Button full width on mobile

--- resources/views/presence/index.blade.php
+++ resources/views/presence/index.blade.php
@@ -13,9 +13,8 @@
 
             <x-slot name="action">
                 <a href="{{ route('employees.index') }}"
-                    class="group w-full sm:w-auto inline-flex items-center justify-center px-5 py-2.5 border border-gray-200 sm:rounded-lg shadow-sm text-sm font-medium text-indigo-700 bg-white hover:bg-indigo-50 hover:border-indigo-300 hover:shadow transition-all duration-200 ease-in-out">
-                    <span
-                        class="bg-indigo-100 p-1.5 rounded mr-2.5 group-hover:bg-indigo-200 transition-all duration-200">
+                    class="group w-full sm:w-auto inline-flex items-center justify-center px-5 py-2.5 border border-gray-200 sm:rounded-lg shadow-sm text-sm font-medium text-indigo-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <span class="bg-indigo-100 p-1.5 rounded mr-2.5">
                         <x-icons.users class="h-4 w-4 text-indigo-600" />
                     </span>
                     Daftar Karyawan
