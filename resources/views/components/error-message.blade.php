7e71b32923290a2dee0edd76b02707bd8d54f786
Rushel <ruselaq@gmail.com>
2025-04-29 20:52:46 +0800

    feat: replace warning-circle icon with danger icon in error message component

--- resources/views/components/error-message.blade.php
+++ resources/views/components/error-message.blade.php
@@ -1,6 +1,6 @@
 @props(['message'])
 
 <div class="mt-1 flex items-center text-sm text-red-600">
-    <x-icons.warning-circle class="h-4 w-4 mr-1.5" />
+    <x-icons.danger class="h-4 w-4 mr-1.5" />
     {{ $message }}
 </div>
