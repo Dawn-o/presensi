4064672d739f1d01fa90ca486be66819ae2a64ab
Rushel <ruselaq@gmail.com>
2025-04-30 23:21:50 +0800

    fix: remove unnecessary border from status column in monthly calendar table

--- resources/views/components/monthly-calendar.blade.php
+++ resources/views/components/monthly-calendar.blade.php
@@ -75,7 +75,7 @@
                                 Jam
                             </th>
                             <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-100">
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Status
                             </th>
                         </tr>
